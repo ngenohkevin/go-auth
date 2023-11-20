045f867497776a361f118d83b6b76522186218ed
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-20 13:18:01 +0300

    added unique key to email

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "Users" (
-                         "id" uuid PRIMARY KEY,
-                         "username" varchar NOT NULL,
-                         "hashed_password" varchar NOT NULL,
-                         "full_name" varchar NOT NULL,
-                         "email" varchar NOT NULL,
-                         "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
-                         "created_at" timestamptz NOT NULL DEFAULT (now())
+  "id" uuid PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
