ddc5ada859c0e11ca010c605aeef47b84abc20a5
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-22 13:24:38 +0300

    random generated input for testing db

---
+++ utils/random.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@gmail.com", RandomString(7))
+}
+
+func RandomUser() string {
+	return fmt.Sprintf("%s", RandomString(8))
+}
