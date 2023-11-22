516c13fdbf363b1fb69474664b1f75b05798a2fe
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-22 13:28:27 +0300

    create a random user

--- db/tests/user_test.go
+++ db/tests/user_test.go
@@ -1,10 +1,27 @@
 package tests
 
 import (
+	"context"
 	db "github.com/ngenohkevin/go-auth/db/sqlc"
+	"github.com/ngenohkevin/go-auth/utils"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func createRandomUser(t *testing.T) db.User {
-	return db.User{}
+	hashedPassword, err := utils.HashPassword(utils.RandomString(8))
+	require.NoError(t, err)
+	arg := db.CreateUserParams{
+		Username:       utils.RandomUser(),
+		Email:          utils.RandomEmail(),
+		FullName:       utils.RandomUser(),
+		HashedPassword: hashedPassword,
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+
 }
