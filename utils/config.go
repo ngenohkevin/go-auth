d9e4871c58b4fae905514a33fa4b1381bacbb36a
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-22 13:23:09 +0300

    connecting to DB

---
+++ utils/config.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+// Config stores all configuration of the application
+type Config struct {
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	ServerAddress        string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
+
+// LoadConfig loads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
