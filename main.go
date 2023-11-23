f31940d0125d50e3ddca0b903c3e6c61e4010112
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-23 13:38:04 +0300

    run the api server

--- main.go
+++ main.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/ngenohkevin/go-auth/api"
+	store "github.com/ngenohkevin/go-auth/db/store"
+	"github.com/ngenohkevin/go-auth/utils"
+	"log"
+)
+
 func main() {
-	//test docker temp
-	println("This is a go-auth app!")
+	config, err := utils.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	connPool, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	stores := store.NewStore(connPool)
+
+	server, err := api.NewServer(config, &stores)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+
 }
