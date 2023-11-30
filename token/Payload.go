92e6c30049469e31b470afe5f983f25fa121f9d6
ngenohkevin <ngenohkevin19@gmail.com>
2023-11-30 12:50:54 +0300

    New payload for tokens

---
+++ token/Payload.go
@@ -0,0 +1,30 @@
+package token
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+// Payload contains the payload data of a token
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates a new payload for a token
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
