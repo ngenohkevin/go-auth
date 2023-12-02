33ce0bba0f4d1c3509a3dbe8ec012b014bf804dd
ngenohkevin <ngenohkevin19@gmail.com>
2023-12-02 12:59:35 +0300

    new payload

--- token/payload.go
+++ token/payload.go
@@ -1,30 +1,44 @@
 package token
 
 import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 	"time"
 )
 
-// Payload contains the payload data of a token
+var (
+	ErrExpiredTokens = errors.New("token has expired")
+	ErrInvalidToken  = errors.New("token is invalid")
+)
+
+// Payload contains the payload data of the token
 type Payload struct {
 	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	IssuedAt  time.Time `json:"issued_at"`
-	ExpiredAt time.Time `json:"expired_at"`
+	ExpiresAt time.Time `json:"expires_at"`
+	jwt.RegisteredClaims
 }
 
-// NewPayload creates a new payload for a token
 func NewPayload(username string, duration time.Duration) (*Payload, error) {
 	tokenID, err := uuid.NewRandom()
 	if err != nil {
 		return nil, err
 	}
-
 	payload := &Payload{
 		ID:        tokenID,
 		Username:  username,
 		IssuedAt:  time.Now(),
-		ExpiredAt: time.Now().Add(duration),
+		ExpiresAt: time.Now().Add(duration),
 	}
+
 	return payload, nil
 }
+
+//func (payload *Payload) Valid() error {
+//	if time.Now().After(payload.ExpiresAt) {
+//		return ErrExpiredTokens
+//	}
+//	return nil
+//}
